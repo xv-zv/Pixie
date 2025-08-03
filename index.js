032ec64b5df77553adb35c85d89ef54c887822cc
xv-zv <sv.yos.n@gmail.com>
2025-08-02 22:09:47 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const { Socket } = require('./Socket')
 const Utils = require('./Utils')
 
-const load = new Utils.Load()
-load.start()
+
 
 //global assign
-global.F =  Utils.Funcs()
-Object.assign(global,{
+global.F = Utils.Funcs()
+Object.assign(global, {
    origen: __dirname,
    core: F.rdJsnSync('./Socket/config.json'),
-   ...load.files
 })
 
 async function start() {
    
+   const load = new Utils.Load()
+   load.start()
+   Object.assign(global, load.files)
    const args = await Utils.Initation()
    
    const bot = new Socket({
@@ -22,10 +23,10 @@ async function start() {
    })
    
    bot.ev.on('code', code => {
-      console.log('code: '+ code)
+      console.log('code: ' + code)
    })
    bot.ev.on('status', status => {
-      console.log('status: '+ status)
+      console.log('status: ' + status)
    })
    
    bot.ev.on('text', (...args) => {
