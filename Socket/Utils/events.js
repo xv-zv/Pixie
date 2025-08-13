4649974804a002c234a653a5e533aabbd41884da
xv-zv <sv.yos.n@gmail.com>
2025-08-13 17:10:52 -0500

    update events.js

--- Socket/Utils/events.js
+++ Socket/Utils/events.js
@@ -1,39 +1,38 @@
 class Events {
    
    constructor() {
-      this.events = {}
-      this.commands = {}
+      this.#events = {}
+      this.#commands = {}
    }
    
    on = (event, listener) => {
-      if (this.events[event]) return 
-      this.events[event] = listener
+      if (this.#events[event]) return
+      this.#events[event] = listener
       return this
    }
    
-   off = (event, ...args) => {
-      if (!this.events[event]) return
-      if (args.length > 0) {
-         this.emit(event, ...args)
-      }
-      delete this.events[event]
-   }
-   
    command = (command, listener) => {
-      if (this.commands[command]) return
-      this.commands[command] = listener
+      if (this.#commands[command]) return
+      this.#commands[command] = listener
    }
    
-   emitCommand = (command, ...args) => {
-      if (!this.commands[command]) return
-      this.commands[command](...args)
+   emitCmd = (command, ...args) => {
+      if (!this.#commands[command]) return
+      this.#commands[command](...args)
    }
    
    emit = (event, ...args) => {
-      if (!this.events[event]) return
-      this.events[event](...args)
+      if (!this.#events[event]) return
+      this.#events[event](...args)
    }
    
+   off = (event, ...args) => {
+      if (!this.#events[event]) return
+      if (args.length > 0) {
+         this.emit(event, ...args)
+      }
+      delete this.#events[event]
+   }
 }
 
 module.exports = { Events }
