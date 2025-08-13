55d76675cb6d704721001208b1a198953829fe50
xv-zv <sv.yos.n@gmail.com>
2025-08-13 17:52:25 -0500

    update sms.js

--- Socket/Utils/sms.js
+++ Socket/Utils/sms.js
@@ -6,11 +6,9 @@ const {
 
 exports.sms = async (sock, ctx, m = {}) => {
    
-   m = {
-      from: ctx.key.remoteJid,
-      chat: jidNormalizedUser(ctx.key.participant || m.from),
-      isGroup: isJidGroup(m.from)
-   }
-   
+   m.from = ctx.key.remoteJid
+   m.chat = jidNormalizedUser(ctx.key.participant || m.from)
+   m.isGroup = isJidGroup(m.from)
+
    return m
 }
