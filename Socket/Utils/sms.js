82000e94d1b9dd7a14f11f199b32e32623f3289d
xv-zv <sv.yos.n@gmail.com>
2025-08-13 18:18:50 -0500

    update sms.js

--- Socket/Utils/sms.js
+++ Socket/Utils/sms.js
@@ -8,7 +8,6 @@ const {
 exports.sms = async (sock, ctx, m = {}) => {
    
    const dv = () => getDevice(ctx.key.id)
-   
    const bot = {
       id: jidNormalizedUser(sock.user?.id),
       lid: jidNormalizedUser(sock.user?.lid),
@@ -20,9 +19,11 @@ exports.sms = async (sock, ctx, m = {}) => {
    
    m.isBot = ctx.key.fromMe && dv() == 'web'
    m.isMe = ctx.key.fromMe && !m.isBot
-   const user = ctx.key.participant
+   const user = ctx.key.participant || m.from
+   m.isUser = !isBot || !m.isMe
    m.isLid = isLidUser(user)
-   m.chat = jidNormalizedUser(user || (m.isBot || m.isMe) ? (m.isLid ? bot.lid : bot.id) : user)
+   
+   m.chat = jidNormalizedUser(m.isGroup ? user : !m.isUser ? (m.isLid ? bot.lid : bot.id) : user)
    
    return m
 }
