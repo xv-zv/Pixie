6028a112c64c3d1db8b0ea31c0d589d99790141d
xv-zv <sv.yos.n@gmail.com>
2025-08-13 18:12:56 -0500

    update sms.js

--- Socket/Utils/sms.js
+++ Socket/Utils/sms.js
@@ -1,14 +1,28 @@
 const {
    jidNormalizedUser,
    getContentType,
+   isLidUser,
    isJidGroup
 } = require('@whiskeysockets/baileys')
 
 exports.sms = async (sock, ctx, m = {}) => {
    
+   const dv = () => getDevice(ctx.key.id)
+   
+   const bot = {
+      id: jidNormalizedUser(sock.user?.id),
+      lid: jidNormalizedUser(sock.user?.lid),
+      name: sock.user?.name || 'annonymous'
+   }
+   
    m.from = ctx.key.remoteJid
-   m.chat = jidNormalizedUser(ctx.key.participant || m.from)
    m.isGroup = isJidGroup(m.from)
-
+   
+   m.isBot = ctx.key.fromMe && dv() == 'web'
+   m.isMe = ctx.key.fromMe && !m.isBot
+   const user = ctx.key.participant
+   m.isLid = isLidUser(user)
+   m.chat = jidNormalizedUser(user || (m.isBot || m.isMe) ? (m.isLid ? bot.lid : bot.id) : user)
+   
    return m
 }
