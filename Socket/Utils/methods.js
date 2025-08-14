693a0deac1dca325dbdd6bf66b1c78f7b7d0df5c
xv-zv <sv.yos.n@gmail.com>
2025-08-14 11:31:23 -0500

    update methods.js

--- Socket/Utils/methods.js
+++ Socket/Utils/methods.js
@@ -1,6 +1,15 @@
 const {
    isJidGroup
 } = require('@whiskeysockets/baileys')
+const {
+   fileTypeFromStream,
+   fileTypeFromBuffer
+} = require('file-type')
+const long = require('long').fromNumber
+const fs = require('fs-extra/promises')
+
+const nmMedia = url => Buffer.isBuffer(url) ? url : { url }
+const nmDesc = txt => typeof txt == 'string' ? txt : txt.desc
 
 class Methods {
    online
@@ -14,6 +23,55 @@ class Methods {
       this.#sock.ev.removeAllListeners()
       this.#sock.ws.close()
       this.online = false
+   }
+   
+   sendMessage = (id, content, opc = {}) => {
+      if (!this.online) return
+      return this.#sock.sendMessage(id, {
+         ...content,
+         contextInfo: {
+            expiration: opc.expiration || 0,
+            mentionedJid: opc.mentions || []
+         }
+      }, { quoted: opc.quoted })
+   }
+   
+   sendImage = (id, content, opc = {}) => {
+      return this.sendMessage(id, {
+         image: nmMedia(content),
+         caption: nmDesc(opc),
+         mimetype: opc.mime || 'image/jpeg',
+         viewOnce: Boolean(opc.once)
+      }, opc)
+   }
+   
+   sendVideo = (id, content, opc = {}) => {
+      return this.sendMessage(id, {
+         video: nmMedia(content),
+         caption: nmDesc(opc),
+         mimetype: opc.mime || 'video/mp4',
+         viewOnce: Boolean(opc.once)
+      }, opc)
+   }
+   
+   sendAudio = (id, content, opc = {}) => {
+      return this.sendMessage(id, {
+         audio: nmMedia(content),
+         mimetype: opc.mime || 'audio/mpeg',
+         ptt: Boolean(opc.note),
+         viewOnce: Boolean(opc.once)
+      }, opc)
+   }
+   
+   sendFile = async (id, media, opc = {}) => {
+      const { ext, mime } = await this.fileType(media)
+      return this.sendMessage(id, {
+         document: nmMedia(media),
+         caption: nmDesc(opc),
+         fileName: (opc.name || mime.split('/')[0]) + '.' + ext,
+         mimetype: mime,
+         fileLength: opc.size ? long(Number(opc.size) * 1000000, true) : null
+      }, opc)
    }
    
    getMetadata = async id => {
@@ -35,6 +93,21 @@ class Methods {
          admins,
          users,
          desc: data.desc
+      }
+   }
+   
+   getFileType = async input => {
+      const isUrl = /https?:\/\/[^\s"'`]+/.test(input)
+      const isBuffer = Buffer.isBuffer(input)
+      const isPath = /[\w\d./-]\.\w{2,4}/.test(input)
+      if (isUrl) {
+         const res = await fetch(input)
+         return fileTypeFromStream(res.body)
+      }
+      if (isBuffer) return fileTypeFromBuffer(input)
+      if (isPath) {
+         const buffer = await fs.readFile(input)
+         return fileTypeFromBuffer(buffer)
       }
    }
 }
