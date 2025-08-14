a45283335bc68993a5bdb5a24e6ff581a20bf421
xv-zv <sv.yos.n@gmail.com>
2025-08-14 12:48:00 -0500

    update sms.js

--- Socket/Utils/sms.js
+++ Socket/Utils/sms.js
@@ -136,7 +136,7 @@ exports.sms = async (sock, ctx, q) => {
          }
       }
       
-      reply = (text, opc = {}) => sock.sendMessage(opc.id || from, { text }, { ephemeral: m.ephemeral || 0, quoted: ctx, ...opc })
+      m.reply = (text, opc = {}) => sock.sendMessage(opc.id || from, { text }, { ephemeral: m.ephemeral || 0, quoted: ctx, ...opc })
    }
    
    return m
